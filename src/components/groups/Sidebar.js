9e65540e88db0f56a19a1aba10b8f4ba9bf6a188
VOHONGKHANG <vohongkhang202@gmail.com>
2023-10-22 16:56:06 +0700

    UI GROUP

--- src/components/groups/Sidebar.js
+++ src/components/groups/Sidebar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import useTheme from '../../context/ThemeContext';
 import './Sidebar.css';
 import { useHistory } from 'react-router-dom';
-import { Add, Search } from '@material-ui/icons';
+import { Search, Settings, RssFeed, Explore, People } from '@material-ui/icons';
 import PostGroupApi from '../../api/postGroups/PostGroupApi';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -20,16 +20,16 @@ const Sidebar = ({ user }) => {
 	const listAccountAction = [
 		{
 			postGroupName: 'Bảng feed của bạn',
-			avatarGroup: 'https://s.net.vn/7mBE',
+			avatarGroup: <RssFeed style={{ fontSize: '25px', margin: 'auto' }} />,
 		},
 		{
 			postGroupName: 'Khám phá',
-			avatarGroup: 'https://s.net.vn/7mBE',
+			avatarGroup: <Explore style={{ fontSize: '25px', margin: 'auto' }} />,
 			onClick: discoverHandler,
 		},
 		{
 			postGroupName: 'Nhóm của bạn',
-			avatarGroup: 'https://s.net.vn/7mBE',
+			avatarGroup: <People style={{ fontSize: '25px', margin: 'auto' }} />,
 			onClick: ownerGroupHandler,
 		},
 	];
@@ -57,13 +57,31 @@ const Sidebar = ({ user }) => {
 	return (
 		<>
 			<Space className="topSidebar" direction="vertical">
+				<div className="topSidebar--setting">
+					<div className="group--infor-introduce">Nhóm</div>
+					<div className="icon-setting">
+						<Settings
+							className="topSidebar--setting-icon"
+							style={{
+								fontSize: '25px',
+							}}
+						/>
+					</div>
+				</div>
 				<div className="topSidebar__search">
-					<Search className="icon__search" />
-					<input type="text" placeholder="Tìm kiếm" className="input__search" />
+					<Search
+						className="icon__search"
+						style={{
+							marginLeft: '10px',
+							fontSize: '24px',
+						}}
+					/>
+					<input type="text" placeholder="Tìm kiếm nhóm" className="input__search" />
 				</div>
 				<div className="topSidebar__createGroup">
 					<Button type="primary" block className="topSidebar__button">
-						<Add /> Tạo nhóm
+						<span className="icon--plus">+</span>
+						<span className="createGroup--text">Tạo nhóm mới</span>
 					</Button>
 				</div>
 			</Space>
@@ -95,7 +113,11 @@ const Sidebar = ({ user }) => {
 											onClick={item.onClick}
 										>
 											<Space align="center" style={{ width: '100%' }}>
-												<img src={item.avatarGroup} alt="avatar_group" className="image--group" />
+												<img
+													src={item.avatarGroup}
+													alt="avatar_group"
+													className="image--group"
+												/>
 												<Typography.Text strong>{item.title}</Typography.Text>
 											</Space>
 										</Button>
@@ -107,10 +129,13 @@ const Sidebar = ({ user }) => {
 										style={{ height: 'auto', padding: '8px' }}
 										onClick={item.onClick}
 									>
-										<Space align="center" style={{ width: '100%' }}>
-											<img src={item?.avatarGroup ?item?.avatarGroup: noAvatar} alt="avatar_group" className="image--group" />
-
-											<Typography.Text strong>{item.postGroupName}</Typography.Text>
+										<Space align="center" style={{ width: '100%', marginLeft: '10px' }}>
+											<div className="icon-setting">{item.avatarGroup}</div>
+											<div style={{ marginTop: '14px' }}>
+												<Typography.Text strong >
+													<div style={{ fontSize: '15px' }}>{item.postGroupName}</div>
+												</Typography.Text>
+											</div>
 										</Space>
 									</Button>
 								)}
